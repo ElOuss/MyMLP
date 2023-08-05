1f0778890fabd81844a407fa19b559e4080ae0ed
Oussama EL Habachi <oussama.elhabachi@gmail.com>
2023-08-05 17:27:50 +0200

    fix bug Scrap.py :bug:

--- Scrap.py
+++ Scrap.py
@@ -2,14 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url = "https://www.google.com/search?sxsrf=AB5stBiCsky1e-hJDKTRHgS31usKRxe1Pw:1691240841574&q=candy+floss&tbm=isch&source=lnms&sa=X&ved=2ahUKEwi5ut2My8WAAxXjUaQEHY_ID0wQ0pQJegQIDxAB&biw=1440&bih=726&dpr=2"
-
+url = "https://www.google.com/search?q=cotton+candy+photoshoot&tbm=isch&ved=2ahUKEwj657uR6sWAAxWInCcCHZHKAZ8Q2-cCegQIABAA&oq=cotton+candy+photosh&gs_lcp=CgNpbWcQARgAMgcIABATEIAEMgcIABATEIAEMggIABAFEB4QEzIICAAQCBAeEBM6BAgjECc6CAgAEAcQHhATOgUIABCABDoECAAQHlDjBljYLGCkM2gAcAB4AIABRIgBogaSAQIxM5gBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=FWrOZLoJiLmewQ-RlYf4CQ&bih=726&biw=1440"
 def getdata(link):
     r = requests.get(link)
     return r.text
 
 htmldata = getdata(url)
-soup = BeautifulSoup(htmldata, 'html.praser')
+soup = BeautifulSoup(htmldata, 'html.parser')
 
 
 for item in soup.findAll('img'):
