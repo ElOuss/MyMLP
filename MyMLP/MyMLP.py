202500df334038daaff8be4882d19077e8b3c511
Oussama EL Habachi <oussama.elhabachi@gmail.com>
2023-09-11 20:10:44 +0200

    ajout fonction flatten_image

--- MyMLP/MyMLP.py
+++ MyMLP/MyMLP.py
@@ -9,9 +9,10 @@ class MyMLP:
         self.struct = npl
         self.layers = len(npl) - 1
         self.weights = []
+        self.output_value = []
         self.loss =[]
 
-        # Initialize weights of the model between -1 and 1 (except for unused weights set to 0)
+        # Initialisation des poids du modèle entre -1 et 1 (sauf pour les poids inutilisés fixés à 0)
         for layer in range(self.layers + 1):
             self.weights.append([])
 
@@ -24,14 +25,13 @@ class MyMLP:
                     self.weights[layer][prev_neurons].append(
                         0.0 if curr_neuron == 0 else np.random.uniform(-1.0, 1.0))
 
-        # Create memory space to store neuron output values
-        self.X = []
+        # Création d'un espace mémoire pour stocker les valeurs de sortie des neurones
         for layer in range(self.layers + 1):
-            self.X.append([])
+            self.output_value.append([])
             for neuron in range(npl[layer] + 1):
-                self.X[layer].append(1.0 if neuron == 0 else 0.0)
+                self.output_value[layer].append(1.0 if neuron == 0 else 0.0)
 
-        # Creating memory space to store semi-gradients associated with each neuron
+        # Création d'un espace mémoire pour stocker les semi-gradients associés à chaque neurone
         self.deltas = []
         for layer in range(self.layers + 1):
             self.deltas.append([])
@@ -39,64 +39,64 @@ class MyMLP:
                 # Initializing the semi-gradients as 0.0
                 self.deltas[layer].append(0.0)
 
-    # Propagation and updating of neuron output values from example inputs
+    # Propagation et mise à jour des valeurs de sortie des neurones à partir des entrées
     def _propagate(self, inputs: List[float], is_classification: bool):
-        # Copying the input values into the input layer of the model
+        # Copie des valeurs d'entrée dans la couche d'entrée du modèle
         for neuron in range(self.struct[0]):
-            self.X[0][neuron + 1] = inputs[neuron]
+            self.output_value[0][neuron + 1] = inputs[neuron]
 
-        # Recursively updating neuron output values, layer by layer
+        # Mise à jour récursive des valeurs de sortie des neurones, couche par couche
         for layer in range(1, self.layers + 1):
             for neuron in range(1, self.struct[layer] + 1):
                 total = 0.0
                 for prev_neuron in range(0, self.struct[layer - 1] + 1):
-                    # Summing the weighted inputs from the previous layer
-                    total += self.weights[layer][prev_neuron][neuron] * self.X[layer - 1][
+                    # Somme des entrées pondérées de la couche précédente
+                    total += self.weights[layer][prev_neuron][neuron] * self.output_value[layer - 1][
                         prev_neuron]
 
-                # Applying the hyperbolic tangent (tanh) activation function if not in the last layer or if it's a classification task
+                # Application de la fonction d'activation (tanh) si ce n'est pas la dernière couche ou si c'est une tâche de classification
                 if layer < self.layers or is_classification:
                     total = np.tanh(total)
 
-                # Updating the neuron's output value
-                self.X[layer][neuron] = total
+                # Mise à jour de la valeur de sortie du neurone
+                self.output_value[layer][neuron] = total
 
-    # Method for querying the model (inference)
+    # Méthode pour interroger le modèle
     def predict(self, inputs: List[float], is_classification: bool):
         self._propagate(inputs, is_classification)
-        return self.X[self.layers][1:]
+        return self.output_value[self.layers][1:]
 
-    # Method for training the model from a labeled dataset
+    # Méthode pour entraîner le modèle à partir d'un ensemble de données étiquetées
     def train(self, all_samples_inputs: List[List[float]], all_samples_expected_outputs: List[List[float]],
               is_classification: bool, iteration_count: int, alpha: float):
-        # For a certain number of iterations
+        # Pour un certain nombre d'itérations
         for it in range(iteration_count):
-            # Choosing a labeled example at random from the dataset
+            # Choix d'un exemple étiqueté au hasard dans l'ensemble de données
             k = np.random.randint(0, len(all_samples_inputs))
             inputs_k = all_samples_inputs[k]
             y_k = all_samples_expected_outputs[k]
 
-            # Updating the neuron output values of the model from the inputs of the selected example
+            # Mise à jour des valeurs de sortie des neurones du modèle à partir des entrées de l'exemple sélectionné
             self._propagate(inputs_k, is_classification)
 
-            # Calculating the semi-gradients of neurons in the layer
+            # Calcul des semi-gradients des neurones dans la couche
             for neuron_idx in range(1, self.struct[self.layers] + 1):
                 self.deltas[self.layers][neuron_idx] = (
-                        self.X[self.layers][neuron_idx] - y_k[neuron_idx - 1])
+                        self.output_value[self.layers][neuron_idx] - y_k[neuron_idx - 1])
                 if is_classification:
                     self.deltas[self.layers][neuron_idx] *= (
-                            1 - self.X[self.layers][neuron_idx] ** 2)
+                            1 - self.output_value[self.layers][neuron_idx] ** 2)
 
-            # Calculating the semi-gradients of neurons in previous layers recursively
+            # Calcul des semi-gradients des neurones dans les couches précédentes de manière récursive
             for layer in reversed(range(1, self.layers + 1)):
                 for prev_neuron_idx in range(1, self.struct[layer - 1] + 1):
                     total = 0.0
                     for neuron_idx in range(1, self.struct[layer] + 1):
-                        # Summing the weighted semi-gradients from the next layer
+                        # Somme des semi-gradients pondérés de la couche suivante
                         total += self.weights[layer][prev_neuron_idx][neuron_idx] * self.deltas[layer][
                             neuron_idx]
-                    # Updating the semi-gradient of the current neuron
-                    self.deltas[layer - 1][prev_neuron_idx] = (1 - self.X[layer - 1][
+                    # Mise à jour du semi-gradient du neurone actuel
+                    self.deltas[layer - 1][prev_neuron_idx] = (1 - self.output_value[layer - 1][
                         prev_neuron_idx] ** 2) * total
 
             # Updating the model weights
@@ -104,28 +104,19 @@ class MyMLP:
                 for prev_neuron_idx in range(0, self.struct[layer - 1] + 1):
                     for neuron_idx in range(1, self.struct[layer] + 1):
                         # Updating the weight using gradient descent
-                        self.weights[layer][prev_neuron_idx][neuron_idx] -= alpha * self.X[layer - 1][
+                        self.weights[layer][prev_neuron_idx][neuron_idx] -= alpha * self.output_value[layer - 1][
                             prev_neuron_idx] * self.deltas[layer][neuron_idx]
             if it%1000 ==0.0:
                 total_loss = 0
                 for inputs, expected_outputs in zip(all_samples_inputs, all_samples_expected_outputs):
                     self._propagate(inputs, is_classification = True)
-                    outputs = self.X[self.layers][1:]
+                    outputs = self.output_value[self.layers][1:]
                     example_loss =self.loss_mse(outputs, expected_outputs)
                     total_loss += example_loss
 
                 average_loss = total_loss / len(all_samples_inputs)
                 self.loss.append(average_loss)
 
-    def calculate_loss(self, all_samples_inputs: List[List[float]], all_samples_expected_outputs: List[List[float]]):
-
-        total_loss = 0.0
-        num_samples = len(all_samples_inputs)
-        for sample_input in all_samples_inputs:
-            for sample_output in all_samples_expected_outputs:
-                total_loss += -sample_output * np.log(sample_input) - (1. - sample_output) * np.log(1. - sample_input)
-        average_loss = total_loss / num_samples
-        return average_loss
 
     def loss_entropy(self, y_pred, y_true):
         # Calcule la différence entre les valeurs prédites et les valeurs cibles réelles.
@@ -148,16 +139,6 @@ class MyMLP:
 
         return loss / (2.0 * len(outputs))
 
-    def accuracy_function(y_true, y_pred):
-        correct = 0
-        total = len(y_true)
-
-        for i in range(total):
-            if y_true[i] == y_pred[i]:
-                correct += 1
-        accuracy = float(correct / total)
-
-        return accuracy
 
     def calculate_accuracy(self, all_samples_inputs: List[List[float]],
                            all_samples_expected_outputs: List[List[float]]):
@@ -178,6 +159,24 @@ class MyMLP:
         accuracy = correct_predictions / num_samples
         return accuracy
 
+    def calculate_accuracy_multi_class(self, all_samples_inputs: List[List[float]],
+                           all_samples_expected_outputs: List[List[float]]):
+        correct_predictions = 0
+        num_samples = len(all_samples_inputs)
+
+        for i in range(num_samples):
+            predicted_outputs = np.array(self.predict(all_samples_inputs[i], is_classification=True))
+            expected_outputs_i = all_samples_expected_outputs[i]
+
+            # convertir les valeur predites en labels (0, 1, 2) argmax
+            predicted_class = np.argmax(predicted_outputs)
+            expected_class = np.argmax(expected_outputs_i)
+
+            if predicted_class == expected_class:
+                correct_predictions += 1
+
+        accuracy = correct_predictions / num_samples
+        return accuracy
     def load_images(dataset_path):
         images = []
         labels = []
@@ -190,21 +189,20 @@ class MyMLP:
 
             for image_name in os.listdir(class_path):
                 image_path = os.path.join(class_path, image_name)
-                # Charge les images dans des tableaux numpy
+                # Charge les images dans des numpy arrays
                 image = np.array(plt.imread(image_path))
                 images.append(image)
                 labels.append(class_name)
 
-        # Retourne les listes des images et des labels
         return images, labels
 
     def preprocess_images(images, scale):
-        """Prétraite les images."""
+        #Prétraite les images
         images = [image / scale for image in images]
         return images
 
     def split_dataset(images, labels, train_ratio=0.8):
-        """Découpe le dataset en train set et test set."""
+        #Découpe le dataset en train set et test set
         n_images = len(images)
         n_train_images = int(n_images * train_ratio)
         n_test_images = n_images - n_train_images
@@ -214,35 +212,36 @@ class MyMLP:
         test_labels = labels[n_train_images:]
         return train_images, train_labels, test_images, test_labels
 
-    # Fonction pour sauvegarder le modèle au format JSON
+    # Fonction pour sauvegarder le modèle en JSON
     def save_model_to_json(model, filename):
-        # Crée un dictionnaire contenant la structure du modèle et ses poids
+        # Crée un dictionnaire qui contient la structure du modèle et ses poids
         model_data = {
             "struct": model.struct,
             "weights": model.weights,
         }
-        # Ouvre le fichier JSON en mode écriture
+        # Ouvre le fichier JSON en mode write
         with open(filename, "w") as json_file:
             # Écrit les données du modèle dans le fichier JSON
             json.dump(model_data, json_file)
 
     # Fonction pour charger un modèle depuis un fichier JSON
     def load_model_from_json(filename):
-        # Ouvre le fichier JSON en mode lecture ("r") et l'assigne à la variable json_file.
+        # Ouvre le fichier JSON en mode (read) et l'assigner à la variable json_file.
         with open(filename, "r") as json_file:
-            # Charge les données JSON depuis le fichier et les assigne à la variable model_data.
             model_data = json.load(json_file)
-        # Crée une instance de la classe MyMLP en utilisant la structure du modèle dans model_data.
+
+        # Crée une instance de la classe MyMLP avec la structure du modèle dans model_data.
         loaded_model = MyMLP(model_data["struct"])
-        # Copie les poids du modèle depuis model_data et les assigne à la propriété weights de loaded_model.
+
         loaded_model.weights = model_data["weights"]
+
         # Retourne le modèle chargé depuis le fichier JSON.
         return loaded_model
 
     def flatten_images(images):
         flattened_images = []
         for image in images:
-            # Aplatit chaque image en un vecteur de caractéristiques
+            # Aplatit chaque image
             flattened_image = image.reshape(-1)
             flattened_images.append(flattened_image)
         return flattened_images
--- Rename_image.py
+++ Rename_image.py
@@ -1,28 +1,27 @@
 import os
 
 # Liste des classes
-classes = ["Barbe à papa", "Churros", "Pomme damour"]
+classes = ["Barbe a Papa", "Churros", "Pomme damour"]
 
 # Fonction pour renommer une image
 def rename_image(image_path, class_name, index):
-    # Obtenir le nom original de l'image
     image_name = os.path.basename(image_path)
 
     # Renommer l'image
     new_image_name = f"{class_name}_image{index:03d}"
     os.rename(image_path, os.path.join(os.path.dirname(image_path), new_image_name))
 
+data_dir = "data"
 
 # Itérer sur les classes
 for class_name in classes:
-    # Obtenir le dossier de la classe
-    class_path = os.path.join("/Users/ousselhabachi/Desktop/MyMLP/data", class_name)
+    class_path = os.path.join(data_dir, class_name)
 
+    image_names = os.listdir(class_path)
 
-    # Obtenir la liste des images dans le dossier
-    image_paths = os.listdir(class_path)
+    for index, image_name in enumerate(image_names):
+        image_path = os.path.join(class_path, image_name)
 
-    # Itérer sur les images
-    for index, image_path in enumerate(image_paths):
-        # Renommer l'image
         rename_image(image_path, class_name, index)
+
+print("Images renommées avec succès.")
